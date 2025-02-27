0bdcc1e4125473d4a4b57d489de0919f083fd434
G Ramesh Babu <dearrameshbabu@gmail.com>
2025-02-27 18:26:07 +0530

    package dir chaged

--- simple-springboot-app/src/main/java/com/springboot/StudentApplication.java
+++ simple-springboot-app/src/main/java/com/springboot/StudentApplication.java
@@ -3,17 +3,17 @@ package com.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// @SpringBootApplication
-// public class StudentApplication {
-
-// 	public static void main(String[] args) {
-// 		SpringApplication.run(StudentApplication.class, args);
-// 	}
-// }
-
 @SpringBootApplication
-public class SimpleSpringBootAppApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(SimpleSpringBootAppApplication.class, args);
-    }
+public class StudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentApplication.class, args);
+	}
 }
+
+// @SpringBootApplication
+// public class SimpleSpringBootAppApplication {
+//     public static void main(String[] args) {
+//         SpringApplication.run(SimpleSpringBootAppApplication.class, args);
+//     }
+// }
