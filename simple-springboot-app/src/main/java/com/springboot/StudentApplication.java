1eb61f845da50b43deb0cb9612d81f0d1f2e41b9
G Ramesh Babu <dearrameshbabu@gmail.com>
2025-02-27 18:19:54 +0530

    studentapplication and pom files changed

--- simple-springboot-app/src/main/java/com/springboot/StudentApplication.java
+++ simple-springboot-app/src/main/java/com/springboot/StudentApplication.java
@@ -3,10 +3,17 @@ package com.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class StudentApplication {
+// @SpringBootApplication
+// public class StudentApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StudentApplication.class, args);
-	}
+// 	public static void main(String[] args) {
+// 		SpringApplication.run(StudentApplication.class, args);
+// 	}
+// }
+
+@SpringBootApplication
+public class SimpleSpringBootAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSpringBootAppApplication.class, args);
+    }
 }
